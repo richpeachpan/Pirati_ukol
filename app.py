c694e9ba65436e3282e09f018b18eaecfd7ce261
Richard Panek <xpanek1@fi.muni.cz>
2021-11-10 23:58:15 +0100

    Creating API call /feed.json

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config['JSON_AS_ASCII'] = False
+
+
+class Feed(Resource):
+
+    def get(self):
+        return {'data': 'Pirati'}
+
+
+api.add_resource(Feed, "/feed.json")
 
 if __name__ == '__main__':
     app.run(port=8080)
