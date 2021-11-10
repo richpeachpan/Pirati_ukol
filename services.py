16ac634adacddce214d69bfdf28e08913b3d6bdd
Richard Panek <xpanek1@fi.muni.cz>
2021-11-11 00:11:25 +0100

    Adding types and docs to services

--- services.py
+++ services.py
@@ -1,13 +1,24 @@
-from flask import jsonify
+from flask import jsonify, Response
 from flask_restful import Resource
 
 from database import DataBase
 
 
 class Feed(Resource):
+    """
+    Service for feed
+    """
 
-    def __init__(self):
+    def __init__(self: 'Feed') -> None:
         self.__data_source = DataBase()
 
-    def get(self):
+    def get(self) -> Response:
+        """
+        Feed get service
+
+        It processed data from data source and return them as
+        flask.Response. It supports JSON.
+
+        :return: flask.Response processed data from data source
+        """
         return jsonify(self.__data_source.data)
