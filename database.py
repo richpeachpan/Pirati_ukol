4fe55e14956624212ae597ab18ddc62ca0883359
Richard Panek <xpanek1@fi.muni.cz>
2021-11-11 00:03:48 +0100

    Adding Exception handling to database

--- database.py
+++ database.py
@@ -1,9 +1,19 @@
+import traceback
 import urllib.error
 import urllib.request
 
 import xmltodict
 
 URL: str = "https://www.pirati.cz/feed.xml"
+
+
+class DataBaseException(Exception):
+
+    def __init__(self: 'DataBaseException', message: str):
+        self.text: str = message
+
+    def __str__(self: 'DataBaseException'):
+        return str(self.__class__) + ": " + self.text
 
 
 class DataBase:
@@ -14,6 +24,16 @@ class DataBase:
     """
 
     def __init__(self: 'DataBase') -> None:
-        with urllib.request.urlopen(URL) as origin:
-            resource: bytes = origin.read()
+        try:
+            with urllib.request.urlopen(URL) as origin:
+                resource: bytes = origin.read()
+        except urllib.error.HTTPError as exception:
+            raise DataBaseException("HTTP error while working with DB:" + str(
+                exception.code))
+        except urllib.error.URLError as exception:
+            raise DataBaseException("URL error while working with DB:" +
+                                    exception.reason)
+        except Exception:
+            raise DataBaseException("Unknown error while working with DB" +
+                                    traceback.format_exc())
         self.data = xmltodict.parse(resource.decode('utf-8'))
