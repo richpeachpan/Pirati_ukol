4a0f1a2ff3b40a4281aacac63e8bb390ad00a66d
Richard Panek <xpanek1@fi.muni.cz>
2021-11-11 16:36:03 +0100

    Remaking article entity

--- entity.py
+++ entity.py
@@ -14,8 +14,22 @@ class Article:
 
     Stands for article and stores all information about article.
     """
-    def __init__(self, info: Dict[str, str]):
-        self.info: Dict[str, str] = info
+
+    def __init__(self, data: Dict[str, str]):
+        self.title: str = data['title']['#text']
+        self.url: str = data['link']['@href']
+        self.id: str = data['id']
+        self.published: str = data['published']
+        self.updated: str = data['updated']
+        self.content: str = data['content']['#text']
+        self.author: str = data['author']['name']
+        self.categories: List[str] = \
+            [category['@term'] for category in data['category']]
+        self.summary: str = data['summary']['#text']
+        self.thumbnail: str = data['media:thumbnail']['@url']
+        self.media_content: Dict[str, str] = \
+            {'type': data['media:content']['@medium'],
+             'url': data['media:content']['@url']}
 
 
 class FeedPage:
@@ -24,6 +38,8 @@ class FeedPage:
 
     Stands for FeedPage and stores info about page and all articles on page
     """
+
     def __init__(self, info: Dict[str, str], entry: List[Article]):
         self.info: Dict[str, str] = info
         self.entry: List[Article] = entry
+        self.entry_len: int = len(entry)
